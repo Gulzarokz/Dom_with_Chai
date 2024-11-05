2b2c5eb813b5dd58616d42483ea46895361c2998
Gulzarokz <gokz22301@gamil.com>
2024-11-05 20:36:21 +0500

    selector and nodeList in DOm

--- index.js
+++ index.js
@@ -11,4 +11,58 @@
 
 
 
+// const element = document.getElementById("title");
+// console.log(element);
+
+
+// const element = document.getElementById("title").id;
+// console.log(element);
+
+// const element = document.getElementById("title").class;
+// console.log(element);
+
+// const element = document.getElementById("title").getAttribute('id');
+// console.log(element);
+
+// const element = document.getElementById("title").getAttribute('gul');
+// console.log(element);
+
+
+//get Attributes
+// const element = document.getElementById('title').getAttribute('class')
+// console.log(element);
+
+//set Attributes
+// const element = document.getElementById('title').setAttribute('class','myTeam')
+// console.log(element);
+
+// const element = document.getElementById('title').style.background = 'blue';
+
+// how to get innerHtml innerText and innerContent
+// const element = document.getElementById('title').innerHTML;
+// console.log(element);
+
+// const element = document.getElementById('title').innerText;
+
+// // const element = document.getElementById('title').textContent; it will used to show in text with in heading like text in span 
+// console.log(element);
+
+// get element by class name 
+// const element = document.getElementsByClassName('gul');
+// console.log(element);
+
+
+//querry selector and querry selector All
+// const element = document.querySelector('h3');
+// console.log(element);
+
+// const element = document.querySelector('#title');
+// console.log(element);
+
+const myUl = document.querySelector('ul');
+console.log(myUl);
+
+
+
+
 
