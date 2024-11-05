fa1453c86bd18818cbaee5bae642d64a8ad35aa6
Gulzarokz <gokz22301@gamil.com>
2024-11-05 12:14:08 +0500

    introduction of Dom

---
+++ index.js
@@ -0,0 +1,14 @@
+// console.log(window);
+// console.log(document);
+// console.log(document.body);
+// console.log(document.baseURI);
+// console.log(document.head);
+
+// // getElements
+
+// const elements = document.getElementById('gul').innerHTML = "<h1>Self learing</h1>";
+// console.log(elements);
+
+
+
+
